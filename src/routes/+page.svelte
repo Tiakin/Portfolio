ad81b2bfb729c90123b0369294a3565c10e78374
Tiakin <killian.duboucher@outlook.fr>
2025-02-11 15:59:56 +0100

    update base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div
+	class="flex min-h-screen flex-col items-center bg-gradient-to-b from-black to-gray-900 text-white"
+>
+	<div class="max-w-lg text-center">
+		<p class="mb-4">
+			Étudiant en BUT Informatique à l'IUT Lyon 1, passionné par les nouvelles technologies.
+		</p>
+		<a href="https://github.com/Tiakin" class="mt-4 block rounded-lg bg-gray-800 p-3"
+			>🔗 github.com/Tiakin</a
+		>
+		<a href="#top" class="mt-2 block rounded-lg bg-gray-800 p-3">📄 Mon CV</a>
+	</div>
+</div>
